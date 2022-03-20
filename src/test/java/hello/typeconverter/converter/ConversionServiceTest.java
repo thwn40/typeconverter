7ebd6d6078828da479dd975a418e379311e94c37
thwn40 <62760166+thwn40@users.noreply.github.com>
2022-03-20 21:33:13 +0900

    Initial commit

---
+++ src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,30 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService(){
+
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+
+
+        Assertions.assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        Assertions.assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+        IpPort convert = conversionService.convert(new IpPort("127.0.0.1", 8080), IpPort.class);
+        Assertions.assertThat(conversionService.convert("127.0.0.1:8080", IpPort.class)).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+
+
+
+    }
+}
